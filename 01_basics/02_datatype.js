c605ba988b3e7eacf5affd1ef0dd29b21b295a35
Elorasatapathy <165885164+Elorasatapthy@users.noreply.github.com>
2024-09-12 06:04:02 +0000

    01_basics

--- 01_basics/02_datatype.js
+++ 01_basics/02_datatype.js
@@ -6,3 +6,23 @@ console.log(3
 )//code reaability should be high
 
 console.log("elora")
+
+let name="hitesh"
+let age= 18
+let isLoggedIn =false
+let state;
+
+
+//number =>2 to power 53
+// bigint
+//string=>""
+//boolean=>true/false
+//null=>standalone value
+//undefined =>
+//symbol =>unique
+
+//object
+
+console.log(typeof name);
+console.log(typeof age);
+
