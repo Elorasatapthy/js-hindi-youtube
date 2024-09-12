b9e616d04197acfc5467989ac657e37ee8be5bf0
Elorasatapathy <165885164+Elorasatapthy@users.noreply.github.com>
2024-09-12 05:50:22 +0000

    01_basics

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,22 @@
+const accountId = 144553
+let accountEmail ="elora@google.com"
+var accountPassword ="12345"
+ accountCity="jaipur"
+ let accountState;
+
+ //accountId = 2 //not allowed
+
+ accountEmail ="eses@com"
+ accountPassword ="212122"
+ accountCity="Bengaluru"
+
+ console.log("accountId");
+ /*
+ prefer not  to use var 
+ because of issue in block scope and functional scope
+
+
+ */
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]) 
+
+
---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,8 @@
+"use strict";//treat all js code as new version
+//alert(3+3)
+console.log(3
+    +
+    3
+)//code reaability should be high
+
+console.log("elora")
