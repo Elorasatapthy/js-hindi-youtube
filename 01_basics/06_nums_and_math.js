807cdf4f9d97eeadfb5cfaea1e56adadeba96bde
Elorasatapathy <165885164+Elorasatapthy@users.noreply.github.com>
2024-09-13 07:11:02 +0000

    06_nums_and_math.js

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,33 @@
+// const score= 400
+// console.log(score);//400
+
+// const balance=new Number(100)
+
+// console.log(balance);//Number:100
+
+// console.log(balance.toString().lenth);
+// console.log(balance.toFixed(1));//3
+
+// const otherNumber=123.8966
+
+// console.log(otherNumber.toPrecision(4));//124
+
+// const hundreds =1000000
+// console.log(hundreds.toLocaleString('en-IN'));1,000,000
+
+
+//+++++++++++++++++++++++++++++math+++++++++++++++++++++++++++++
+
+console.log(math);
+console.log(math.abs(-4));
+console.log(math.ceil(4.2));
+console.log(math.floor(4.9));
+console.log(math.min(4,3,6,8));
+console.log(math.random());
+
+console.log((math.random()*10)+1);
+
+const min=10
+const max=20
+
+math.random() *(max-min+1)
