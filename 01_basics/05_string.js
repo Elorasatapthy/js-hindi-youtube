a9b10bfd579d8405bea9c40eddb5ae596f984c61
Elorasatapathy <165885164+Elorasatapthy@users.noreply.github.com>
2024-09-13 05:59:48 +0000

    05_string.js

---
+++ 01_basics/05_string.js
@@ -0,0 +1,30 @@
+const name="hitesh"
+const repoCount =50
+//console.log(name+repoCount +"value");
+
+console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName =new String('hiteshhc')
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.lenth);//8
+// console.log(gameName.toUpeerCase)
+console.log(gameName.charAt(2));//t
+
+const newString =gameName.substring(0,4)
+console.log(newString);//hite
+
+const anotherString=gameName.slice(-8,4)
+console.log(anotherString);//hite
+const newstringOne= "     hitesh     ";
+console.log(newstringOne);
+console.log(newstringOne.trim());
+
+console.log(url.replace('%20','-'))
+
+console.log(url.includes('sunder'))
+console.log(gameName.split('-'));
+
+
+
