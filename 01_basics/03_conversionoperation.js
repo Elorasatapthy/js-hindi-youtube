01a3496970e529484f3c65e1ac4cb178e1c140e9
Elorasatapathy <165885164+Elorasatapthy@users.noreply.github.com>
2024-09-13 04:55:23 +0000

    03_conversionoperation.js

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,59 @@
+let score ="33abc"
+
+console.log(typeof score);//String
+console.log(typeof(score));//string
+
+let valueInNumber =Number(score);
+
+console.log(typeof valueInNumber);//number
+console.log(valueInNumber);
+
+//"33" =>33
+//"33abc"=>NaN
+//true =>1; false =>0
+let isloggedIn =1
+
+let booleanIsLoggedIn = Boolean(isloggedIn)
+console.log(booleanIsLoggedIn);//TRUE
+
+let someNumber =33;
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);//33
+console.log( typeof stringNumber);//33
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+let value =3;
+let negvalue = -value;
+//console.log(negvalue)
+//console.log(2+2)
+//console.log(2*2)
+//console.log(2**3)
+//console.log(2-2)
+//console.log(2/2)
+//console.log(2%2)
+
+let str1="hello";
+let str2="hitesh";
+
+let str3 =str1+str2;//hellohitesh
+console.log(1+"2");//12
+console.log("1"+2);//12
+console.log("1"+2+2);//122
+console.log(1+2+"2");//32
+
+let num1,num2,num3;
+num1=num2=num3=2+2
+let gameCounter =100;
+++gameCounter;
+console.log(gameCounter);
+
+
+
+
+
+
+
+
+
